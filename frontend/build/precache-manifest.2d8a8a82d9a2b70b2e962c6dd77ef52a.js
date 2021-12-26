4cdb0f0079f8d99b133a6e668753c2040eb90150
Shambhavi Singh <77614217+sshambhavii@users.noreply.github.com>
2021-12-26 16:48:16 +0530

    Add files via upload

---
+++ frontend/build/precache-manifest.2d8a8a82d9a2b70b2e962c6dd77ef52a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d70ae4e602d62f19157ad3dd9876b63a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d581daf415e015a14b8",
+    "url": "/static/css/0.10eff1aa.chunk.css"
+  },
+  {
+    "revision": "2ffc76a2f668be2dc941",
+    "url": "/static/css/4.6d344725.chunk.css"
+  },
+  {
+    "revision": "5e81c1fde5be7485e457",
+    "url": "/static/css/5.b957ddbe.chunk.css"
+  },
+  {
+    "revision": "9a9b447d44f220578f33",
+    "url": "/static/css/6.a60aebec.chunk.css"
+  },
+  {
+    "revision": "55b703551cd2bb924928",
+    "url": "/static/css/7.d37dee14.chunk.css"
+  },
+  {
+    "revision": "4c66ba159ed4886b17cf",
+    "url": "/static/css/8.55e8af33.chunk.css"
+  },
+  {
+    "revision": "46ea168e53c78fcbc4ac",
+    "url": "/static/css/main.65269f5a.chunk.css"
+  },
+  {
+    "revision": "5d581daf415e015a14b8",
+    "url": "/static/js/0.38e161da.chunk.js"
+  },
+  {
+    "revision": "2a6b15382c099562dd5c",
+    "url": "/static/js/3.0d52b6e6.chunk.js"
+  },
+  {
+    "revision": "2ffc76a2f668be2dc941",
+    "url": "/static/js/4.4372c1a1.chunk.js"
+  },
+  {
+    "revision": "5e81c1fde5be7485e457",
+    "url": "/static/js/5.b883fa1b.chunk.js"
+  },
+  {
+    "revision": "9a9b447d44f220578f33",
+    "url": "/static/js/6.177b9c6f.chunk.js"
+  },
+  {
+    "revision": "55b703551cd2bb924928",
+    "url": "/static/js/7.c3f4e8a4.chunk.js"
+  },
+  {
+    "revision": "4c66ba159ed4886b17cf",
+    "url": "/static/js/8.f893a4eb.chunk.js"
+  },
+  {
+    "revision": "46ea168e53c78fcbc4ac",
+    "url": "/static/js/main.dfdcda58.chunk.js"
+  },
+  {
+    "revision": "0104b5ebdea1f4672dfc",
+    "url": "/static/js/runtime-main.8264ffdc.js"
+  }
+]);
